93c3ed394e0dddc9cd1fc31e702a5c583f36c53f
CUI_Can <56673370+ccan1995@users.noreply.github.com>
2021-02-15 11:02:04 +0100

    Update BOT_multilingue_translate.py
    L'ajout des commentaires

--- train/BOT_multilingue_translate.py
+++ train/BOT_multilingue_translate.py
@@ -7,17 +7,20 @@ import pandas as pd
 
 translator = google_translator()
 #sentence_bert_model = np.load('model/embedding.npy')
-sentence_bert_model = SentenceTransformer('bert-base-nli-mean-tokens')
+sentence_bert_model = SentenceTransformer('bert-base-nli-mean-tokens') # utiliser un modèle déjà entrainé
 
+# lecture des fichiers avec pandas
 def get_dataframe(path):
     df = pd.read_csv(path, sep=';', names=['Question', 'Type', 'Answer'])
     return df
 
+# diviser les deux listes
 def get_list(df):
     question_list = df['Question'].to_list()
     answer_list = df['Answer'].to_list()
     return question_list, answer_list
 
+# diviser les trois types de questions
 def classify_data(df):
     psycho_frame = df[df['Type'] == 'psychology']
     other_frame = df[df['Type'] == 'not_drug_question']
@@ -25,16 +28,12 @@ def classify_data(df):
         df['Type'] != 'not_drug_question')]
     return psycho_frame, other_frame, drug_frame
 
-# calculate similarity between two vectors
-
-
+# calculer la similarité entre deux vecteurs avec outil de pyTorch
 def get_cosin_sim(vector_a, vector_b):
     #sim = 1 - cosine(vector_a, vector_b)
     return util.pytorch_cos_sim(vector_a, vector_b)
 
-
-# get the biggest similarity and its index in a list of sentence
-
+# retourner la maximum des similarités et son index dans une liste des phrases (questions/réponses)
 def get_similarity_max(vector_a, liste):
     sim_list = []
     for ele in liste:
@@ -45,7 +44,7 @@ def get_similarity_max(vector_a, liste):
     idx = sim_list.index(max_sim)
     return max_sim, idx
 
-
+# retourner la réponse selon question_list ou answer_list
 def get_answer(query_vect, question_list, answer_list):
     sim_query_question_max, idx_qq = get_similarity_max(
         query_vect, question_list)
@@ -56,13 +55,15 @@ def get_answer(query_vect, question_list, answer_list):
     else:
         return answer_list[idx_qr]
 
-
+    
+# différentes inferfaces à connecter avec des modules dans le web frontal
+# salutation de bot
 def welcome_bot():
     content = "Hello, my name is Melody, I'm the baby of mom Shanshan and dad Cancan, I can speak French, Chinese and English, what language do you speak? Please input en or zh or fr "
     print("BOT: ", content)
     # return content
 
-
+# choisir la langue
 def feedback_welcome(language):
     if language == 'fr':
         print("BOT : D'accord, vous avez choisir la langue française, qu'est-ce que je peux vous aider ? ")
@@ -74,27 +75,7 @@ def feedback_welcome(language):
         print("BOT : Okay, you have choisen English, what can I help you with ? ")
         # return "Okay, what can I help you with ? "
 
-
-def feedback_bye(language):
-    if language == 'fr':
-        print('BOT : Au revoir ! Bonne journée !')
-    if language == 'zh':
-        print('BOT : 再见，祝您拥有愉快的一天 ！')
-    if language == 'en':
-        print('BOT : Bye ! Have a nice day !')
-
-# three buttons to classify questions
-
-
-def feedback_type(language):
-    if language == 'fr':
-        print('Veillez choisir le domaine à consulter : psychology, drug, joke, others')
-    if language == 'zh':
-        print('请选择您想咨询的领域 ： 心理咨询（psychology）, 药品咨询(drug), 其他(others)')
-    if language == 'en':
-        print('Please select your type for consulting : psychology, drug, joke, others')
-
-
+# réaction après le choix de langue
 def feedback_question(language):
     if language == 'fr':
         print('maintenant vous pouvez entrer votre question')
@@ -103,7 +84,16 @@ def feedback_question(language):
     if language == 'en':
         print('now you can entrer your questions.')
 
+# trois boutons pour les différents types de questions
+def feedback_type(language):
+    if language == 'fr':
+        print('Veillez choisir le domaine à consulter : psychology, drug, joke, others')
+    if language == 'zh':
+        print('请选择您想咨询的领域 ： 心理咨询（psychology）, 药品咨询(drug), 其他(others)')
+    if language == 'en':
+        print('Please select your type for consulting : psychology, drug, joke, others')
 
+# fonction blague
 def feed_back_joke(language, df_en, df_fr):
     if language == 'fr':
         df = df_fr
@@ -113,7 +103,7 @@ def feed_back_joke(language, df_en, df_fr):
     print(df['Answer'].sample(1))
     # return df['Joke'].sample(1), df['Answer'].sample(1)
 
-
+# chercher dans différentes DataFrame selon le type
 def type_choix(psy_df, other_df, drug_df, df_en, df_fr, language):
     feedback_type(language)
     type_ch = input()
@@ -134,13 +124,19 @@ def type_choix(psy_df, other_df, drug_df, df_en, df_fr, language):
             feedback_question(language)
             return question_list, answer_list
 
-
+# chercher la meilleure réponse en donnant la langue et la requête
 def communication(language, query, question_list, answer_list):
+    # si le langage choisi n'est pas l'anglais, il faut d'abord traduire la requête en anglais
     if not language == 'en':
         query_en = translator.translate(
             query, lang_src=language, lang_tgt='en')
+        
+        # entrer dans le modèle sentenceBERT pour le convertir en vecteurs
         query_vect = sentence_bert_model.encode([query_en])[0]
         answer = get_answer(query_vect, question_list, answer_list)
+        
+        #Dans la question_list, il y a deux types de réponses qui montrent que les réponses ne sont pas disponibles.
+        #dans ce cas-là, on souhaite qu'il puisse retourner une réponse unique et rédigée
         if not answer == 'No answer' or 'Unanswerable':
             answer_translate = translator.translate(
                 answer, lang_src='en', lang_tgt=language)
@@ -159,7 +155,16 @@ def communication(language, query, question_list, answer_list):
     # return "Sorry, I don't know how to answer this question, but I will try hard to learn it."
     # return answer_translate
 
-
+# salutation à la fin
+def feedback_bye(language):
+    if language == 'fr':
+        print('BOT : Au revoir ! Bonne journée !')
+    if language == 'zh':
+        print('BOT : 再见，祝您拥有愉快的一天 ！')
+    if language == 'en':
+        print('BOT : Bye ! Have a nice day !')
+        
+# fonction finale
 def chatbot():
     try:
         datapath = './data/data_final.csv'
@@ -176,6 +181,7 @@ def chatbot():
         question_list, answer_list = type_choix(
             psycho_frame, other_frame, drug_frame, joke_en_df, joke_fr_df, language)
 
+        # une boucle qui permet à donner des requetes sans arrêt
         while True:
             try:
                 query = input()
