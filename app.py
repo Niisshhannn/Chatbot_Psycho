29eec5a589fb3e0b2b9e35752a8c28ba66f541fb
Niisshhannn <tangnishan96@gmail.com>
2021-02-16 00:52:44 +0100

    update app

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+# -*- coding: UTF-8 -*-
+
 from flask import Flask, render_template, request
+from server.chatcore import chat
+
 app = Flask(__name__, static_folder="web/dist/static",
             template_folder="web/dist")
 
@@ -8,15 +12,12 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/api/init')
-def chat_init():
-    return {'test': '111'}
-
-
 @app.route('/api/chat', methods=['POST'])
 def chat_main():
     msg = request.json.get('msg')
-    return {'msg': msg + '!!!!!!'}
+    typ = request.json.get('type')
+    lang = request.json.get('lang')
+    return chat(msg, lang, typ)
 
 
 if __name__ == '__main__':
